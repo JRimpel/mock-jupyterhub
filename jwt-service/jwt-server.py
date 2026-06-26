66f77d4063d69317af47b3e6a34ad912c065ac90
JRimpel <jrimpel@gmail.com>
2026-06-26 14:42:50 -0400

    Working jupyter notebook setup

--- jwt-service/jwt-server.py
+++ jwt-service/jwt-server.py
@@ -15,14 +15,16 @@ class JWTServiceHandler(RequestHandler):
 
     def get(self):
         token = self.create_token('admin')
+        print('TOKEN CREATED AND RETURNING')
         self.write(token)
 
     def create_token(self, username):
+        print('CREATING TOKEN...')
         claims_set = {
             'sub': username,
             'exp': datetime.now() + timedelta(minutes=JWT_EXPIRATION_MINUTES)
         }
-        with open('/run/secrets/jwt-key', 'r') as private_key_file:
+        with open('/run/secrets/jwt-private-key', 'r') as private_key_file:
             return jwt.encode(
                 claims_set,
                 private_key_file.read(),
@@ -30,12 +32,14 @@ class JWTServiceHandler(RequestHandler):
             )
 
 def main():
-    application = Application([('/', JWTServiceHandler)])
+    print('main opened')
+    application = Application([('/', JWTServiceHandler)],debug=True)
 
     http_server = HTTPServer(application)
-    url = urlparse('http://localhost:9888')
+    url = urlparse('http://0.0.0.0:9888')
 
     http_server.listen(url.port, url.hostname)
+    print('server running')
     IOLoop.current().start()
 
 if __name__=='__main__':
